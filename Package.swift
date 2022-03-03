e9178a4b86caf38e0e53f1c74671c43e6b88c262
Cole Moore <Cartisim@icloud.com>
2022-03-03 16:50:57 +0800

    updated binary

--- Package.swift
+++ Package.swift
@@ -11,15 +11,15 @@ let package = Package(
             targets: ["LASDK"]),
     ],
     dependencies: [
-        .package(name: "ACBClientSDK", url: "https://github.com/cbajapan/acb-client-sdk.git", .exact("3.4.3"))
+        .package(name: "objc-fcsdk-ios", url: "https://github.com/cbajapan/objc-fcsdk-ios.git", .exact("3.4.5"))
     ],
     targets: [
         .target(
             name: "LASDK",
             dependencies: [
                 "AssistSDK",
-                .product(name: "ACBClientSDK", package: "ACBClientSDK")
+                .product(name: "ACBClientSDK", package: "objc-fcsdk-ios")
             ]),
-        .binaryTarget(name: "AssistSDK", url: "https://objc-sdk.s3.us-east-2.amazonaws.com/live_assist_sdk/assist_sdk/AssistSDK-1.64.10-master.xcframework.zip", checksum: "ae27551eeaa13c494fa78a341f1ecc8c96c58c2fb9ddf5eb58643bd244e223eb")
+        .binaryTarget(name: "AssistSDK", url: "https://objc-sdk.s3.us-east-2.amazonaws.com/live_assist_sdk/assist_sdk/AssistSDK-1.64.12.xcframework.zip", checksum: "96137c611a5c8d8a070114ee9d5e95159b2ff35633169f66ad71f841649cc2cf")
     
 ])
