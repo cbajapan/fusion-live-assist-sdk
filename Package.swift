3b59d09596984ccedf159f9e74411fbbe023d529
Cole Moore <Cartisim@icloud.com>
2023-11-06 12:51:17 +0400

    Updates for version 1.64.13

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
             targets: ["LASDK"]),
     ],
     dependencies: [
-        .package(name: "objc-fcsdk-ios", url: "https://github.com/cbajapan/objc-fcsdk-ios.git", .exact("3.4.5"))
+        .package(name: "objc-fcsdk-ios", url: "https://github.com/cbajapan/objc-fcsdk-ios.git", .exact("3.4.6-beta1"))
     ],
     targets: [
         .target(
@@ -20,6 +20,6 @@ let package = Package(
                 "AssistSDK",
                 .product(name: "ACBClientSDK", package: "objc-fcsdk-ios")
             ]),
-        .binaryTarget(name: "AssistSDK", url: "https://objc-sdk.s3.us-east-2.amazonaws.com/live_assist_sdk/assist_sdk/AssistSDK-1.64.12.xcframework.zip", checksum: "96137c611a5c8d8a070114ee9d5e95159b2ff35633169f66ad71f841649cc2cf")
+        .binaryTarget(name: "AssistSDK", url: "https://objc-sdk.s3.us-east-2.amazonaws.com/live_assist_sdk/assist_sdk/AssistSDK-1.64.13.xcframework.zip", checksum: "af486c8a7783035ccbb2e69a21c4e856a42717ec0d64a95bd8b300c88bb9f8e5")
     
 ])
